96b1959ca619d3597dabdb4ac3eaf46ad9f91342
wwwblogger1 <wwwblogger1@gmail.com>
2021-07-12 22:57:34 +0300

    projectTriangle4

---
+++ Zfile/Triangle_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Triangle_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Введите высоту треугольника: ");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                Console.SetCursorPosition(height -i, i+1);
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
